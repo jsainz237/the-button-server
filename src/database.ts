0518c4c5f8056eba25f69986229913002d1e94fc
Jesse <jsainz237@gmail.com>
2020-04-30 22:30:25 -0500

    create connection to database

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 
 /** Sequelize connection to postgres database + model detection */
 export const database = new Sequelize({
-    database: 'the-button-db',
+    database: 'the_button_db',
     dialect: 'postgres',
     models: [__dirname + '/models/*.ts']
 })
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ import http from 'http';
 import socketio from 'socket.io';
 
 import { SocketEvent } from './types/events';
+import { database } from './database';
 // import { database } from './database';
 
-const PORT = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 // new express application instance
 const app: express.Application = express();
 const server = new http.Server(app);
 const io = socketio(server);
+
+// sync Database with model declarations
+const forceSyncDB = process.env.npm_config_create_db ? true : false; // force create db if --create_db flag was set
+database.sync({ force: forceSyncDB });
 
 io.on('connection', socket => {
     socket.on(SocketEvent.PRESSED, () => {
@@ -19,6 +24,6 @@ io.on('connection', socket => {
 })
 
 
-server.listen(PORT, function() {
-    console.log(`The button server listening on port ${PORT}`)
+server.listen(port, function() {
+    console.log(`The button server listening on port ${port}`)
 })
