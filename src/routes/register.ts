26b608092e33b7a92034a660f74851c57e7da118
Jesse <jsainz237@gmail.com>
2020-05-02 02:23:51 -0500

    add register validation

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
+import { check, validationResult } from 'express-validator';
 
 import User from '../models/user';
 
@@ -11,19 +12,36 @@ interface RegisterReqBody {
     password: string;
 }
 
-router.post('/', (req: Request<{}, {}, RegisterReqBody>, res: Response) => {
-    const user_id = uuidv4();
-    const username = req.body.username;
-    const ci_username = username.toLowerCase();
+router.post('/', [
+        check('username').isAlphanumeric().withMessage('username must be alphanumeric'),
+        check('password')
+            .isLength({ min: 8, max: 64 }).withMessage('must be 8 to 64 characters long')
+            .matches(/\d/).withMessage('must contain a number')
+            .matches(/[A-Z]/).withMessage('must contain an uppercase letter')
+            .matches(/[a-z]/).withMessage('must contain a lowecase letter')
+            .matches(/[!?(){}_\\-\\.$#&]/).withMessage('must contain a special character')
+            .matches(/^((?!')(?!").)*$/).withMessage('cannot contain \" or \' characters')
+    ], (req: Request<{}, {}, RegisterReqBody>, res: Response) => {
+        
+        // check validation errors
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
 
-    try {
-        const password_encrypted = bcrypt.hashSync(req.body.password, 10);
-        User.create({ id: user_id, ci_username, username, password: password_encrypted });
-        res.status(201).send();
-    } catch(err) {
-        console.log(err);
-        res.status(500).send();
+        const user_id = uuidv4();
+        const username = req.body.username;
+        const ci_username = username.toLowerCase();
+
+        try {
+            const password_encrypted = bcrypt.hashSync(req.body.password, 10);
+            User.create({ id: user_id, ci_username, username, password: password_encrypted });
+            return res.status(201).send();
+        } catch(err) {
+            console.log(err);
+            return res.status(500).send();
+        }
     }
-})
+)
 
 export default router;
