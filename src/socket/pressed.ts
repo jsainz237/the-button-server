4479f8ff360450200d6b8f8d19f56e3fe7546fbb
Jesse <jsainz237@gmail.com>
2020-05-07 18:51:12 -0500

    add resetting colorState in pressed listener

--- src/socket/pressed.ts
+++ src/socket/pressed.ts
@@ -1,8 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
+import { io } from '../index';
+import { colorState } from '../colorState';
 import User from "../models/user";
 import { SocketEvent } from '../types/events';
 
-export async function on_pressed_event(io: SocketIO.Server, user_id?: string) {
+export async function onPressedEvent(user_id?: string) {
     const user = user_id ?
         await User.findOne({
             where: { id: user_id }
@@ -10,5 +12,6 @@ export async function on_pressed_event(io: SocketIO.Server, user_id?: string) {
         null;
 
     const username = user?.username || `Anon-${uuidv4().slice(-5)}`;
+    colorState.reset();
     io.emit(SocketEvent.RESET, username)
 }
