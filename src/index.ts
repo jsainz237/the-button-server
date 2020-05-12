290d3efdaebd21072f566a4e23cab5495a04ac9c
Jesse <jsainz237@gmail.com>
2020-05-11 21:09:12 -0500

    create check username functionality / route

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import express from 'express';
 import http from 'http';
 import socketio from 'socket.io';
 import bodyParser from 'body-parser';
+// import cookieParser from 'cookie-parser';
 
 import { SocketEvent } from './types/events';
 import { database } from './database';
 import registerRoutes from './routes/register';
 import loginRoutes from './routes/login';
+import checkUsernameRoutes from './routes/checkUsername';
 import { onPressedEvent } from './socket/pressed';
 import { Rank } from './types/ranks';
 import { colorState } from './colorState';
+// import { checkCookie } from './utils/cookies';
 
 const port = process.env.PORT || 5000;
 
@@ -29,15 +32,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// parse cookie
+// app.use(cookieParser());
+
 // sync Database with model declarations
 const forceSyncDB = process.env.npm_config_create_db ? true : false; // force create db if --create_db flag was set
 database.sync({ force: forceSyncDB });
 
 // REST routes
-app.use('/auth/register/', registerRoutes);
+app.use('/auth/register', registerRoutes);
 app.use('/auth/login', loginRoutes);
+app.use('/auth/check-username', checkUsernameRoutes);
 
+// Socket connections & events
 io.on('connect', socket => {
+    // checkCookie(socket);
     console.log('client connected');
     if(colorState.isDead) { 
         socket.emit(SocketEvent.DEATH)
---
+++ src/routes/checkUsername.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express';
+import User from '../models/user';
+
+const router = express.Router();
+
+interface CheckUsernameBody {
+    username: string;
+}
+
+// on GET request to /auth/check-username
+router.get('/', async (req: Request<{}, {}, CheckUsernameBody>, res: Response) => {
+    const username = req.body.username.toLowerCase();
+
+    // look for user in database with matching username
+    const user = await User.findOne({
+        where: { ci_username: username }
+    })
+
+    if(user) {
+        // send back response saying username is not available
+        return res.status(200).send({ available: false });
+    } else if (!user) {
+        // send back response saying username is available
+        return res.status(200).send({ available: true });
+    }
+
+    return res.status(500).send({ error: 'internal server error' });
+})
+
+export default router;
